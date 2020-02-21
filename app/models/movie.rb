2630b420d2022a66a54e3231abe8cd3e7990a826
Abishalini <ec2-user@ip-172-31-33-23.us-east-2.compute.internal>
2020-02-21 02:51:03 +0000

    Part 3 test with new def in Movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,7 +6,7 @@ class Movie < ActiveRecord::Base
     def self.all_ratings
         result = {}
         self.select(:rating).uniq.each do |movie|
-            result[movie.rating] = 1
+            result[movie.rating] = ''
         end
         return result
     end
