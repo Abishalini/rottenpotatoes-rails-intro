1ab3052ebe4373424e7d686382da08276a62b1b0
Abishalini <ec2-user@ip-172-31-33-23.us-east-2.compute.internal>
2020-02-21 03:14:09 +0000

    Part 3 test without all_ratings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,6 +15,8 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings
     @sort_by = params[:sort_by] || session[:sort_by]
     
+    #session[:ratings] = session[:ratings] || @all_ratings
+    
     session[:ratings] = session[:ratings] || {'G'=>'', 'PG'=>'','PG-13'=>'','R'=>''}
     
     #params[:ratings].nil? ? @t_param = @all_ratings : @t_param = params[:ratings].keys   
